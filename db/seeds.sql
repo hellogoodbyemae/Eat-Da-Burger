0b7017333598c52932ac465f3e137189d6e2fae4
Margaret Egner <hellogoodbyemae@gmail.com>
2020-07-16 22:29:54 -0400

    created files based off cat app

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Pizza Burger');
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Impossible Burger');
+INSERT INTO burgers (name) VALUES ('Black Bean Burger');
+INSERT INTO burgers (name) VALUES ('Turkey Burger');
